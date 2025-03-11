dec9c4229f51f15beea3b4183a864655e7b1972d
KedzeMcbride <Kedzemcbridekum@gmail.com>
2025-03-11 05:43:17 +0100

    tuesdays first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './assets/Body/Nav'
 import Welcome from './assets/Body/welcome'
+import Services from './assets/Body/service'
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Welcome></Welcome>
+    <Services></Services>
     </>
   )
 }
--- src/assets/Body/service.jsx
+++ src/assets/Body/service.jsx
@@ -1,7 +1,21 @@
 const Services = () => {
     return ( 
         <div className="service">
-            
+            <div className="consultation">
+
+            </div>
+            <div className="treatment">
+
+            </div>
+            <div className="results">
+
+            </div>
+            <div className="secure">
+
+            </div>
+
+            <h3 className="our">Our Services</h3>
+            <h4 className="small">WE PROVIDE DIFFERENT TYPE OF SERVICES</h4>
         </div>
      );
 }
