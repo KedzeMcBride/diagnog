8b31f8bb7b2ca35b4d65c55b2c790d6191857b28
KedzeMcbride <Kedzemcbridekum@gmail.com>
2025-03-23 03:08:09 +0100

    steps and card started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from './assets/Body/Nav'
 import Welcome from './assets/Body/welcome'
 import Services from './assets/Body/service'
+import Process from './assets/Body/process'
+import Card from './assets/Body/card'
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Welcome></Welcome>
     <Services></Services>
+    <Process></Process>
+    <Card></Card>
     </>
   )
 }
---
+++ src/assets/Body/card.jsx
@@ -0,0 +1,15 @@
+const Card = () => {
+    return ( 
+        <div className="card">
+            <p className="heading">
+                Popular this month
+            </p>
+                <p>
+                    Top Doctor
+                </p>
+            <p>Kedze McBride Fubin</p>
+        </div>
+     );
+}
+ 
+export default Card;
