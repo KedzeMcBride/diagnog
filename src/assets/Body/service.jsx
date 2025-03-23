087412707990b39c5fa87fa62986c6dc9465db90
KedzeMcbride <Kedzemcbridekum@gmail.com>
2025-03-23 01:12:33 +0100

    service & css updated

--- src/assets/Body/service.jsx
+++ src/assets/Body/service.jsx
@@ -4,27 +4,34 @@ import treat from '../images/treat.png';
 import confidential from '../images/confidential.png';
 const Services = () => {
     return ( 
+        <div className="service-container">
+        {/* Left Side - Service Boxes */}
         <div className="service">
             <div className="diagnose">
-            <img src={diagnose} alt="" />
-            <h3>Do Your Diagnostics By Yourself</h3>
+                <img src={diagnose} alt="Diagnostics" />
+                <h3>Do Your Diagnostics By Yourself</h3>
             </div>
             <div className="treatment">
-            <img src={consult} alt="" className="consult" />
-            <h3>Book Sessions or Consult Online</h3>
+                <img src={consult} alt="Consult" className="consult" />
+                <h3>Book Sessions or Consult Online</h3>
             </div>
             <div className="results">
-            <img src={treat} alt="" className="treat" />
-            <h3>Get Effective solutions to your skin problems</h3>
+                <img src={treat} alt="Treatment" className="treat" />
+                <h3>Get Effective Solutions to Your Skin Problems</h3>
             </div>
             <div className="secure">
-            <img src={confidential} alt="" className="confidential" />
-            <h3>100% Confidential and Secure</h3>
+                <img src={confidential} alt="Confidential" className="confidential" />
+                <h3>100% Confidential and Secure</h3>
             </div>
-
-            <h3 className="our">Our Services</h3>
-            <h4 className="small">WE PROVIDE DIFFERENT TYPE OF SERVICES</h4>
         </div>
+    
+        {/* Right Side - Text Content */}
+        <div className="service-text">
+            <h3>Our Services</h3>
+            <h4>WE PROVIDE DIFFERENT TYPES OF SERVICES</h4>
+        </div>
+    </div>
+    
      );
 }
  
