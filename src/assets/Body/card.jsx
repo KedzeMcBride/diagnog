8e3680a640b313d7ee0eb9414a09e0e49091dcd4
KedzeMcbride <Kedzemcbridekum@gmail.com>
2025-03-24 05:38:26 +0100

    cards completed

--- src/assets/Body/card.jsx
+++ src/assets/Body/card.jsx
@@ -1,6 +1,7 @@
 const Card = () => {
     return ( 
         <div className="containery">
+            <h2>Top <em>Doctors</em>Of the Week</h2>
         <div className="card">
         <p><span>
             </span></p>
